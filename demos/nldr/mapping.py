a4813685b91a7aa492ecd7f22b974b36a9b9b231
mcneela <daniel.mcneela@gmail.com>
2016-06-18 11:02:28 -0700

    Added new dynamic nldr example

--- demos/nldr/mapping.py
+++ demos/nldr/mapping.py
@@ -14,3 +14,12 @@ def section(data, num_sections, axis='x'):
         print(axis + " is not a valid axis.")
     sections = get_sections(data[data[:, ax].argsort()], num_sections)
     return sections
+
+def progressive_segment(data, num_iterations, axis=0):
+    seg_size = data.shape[0] / num_iterations
+    segs = []
+    for i in range(num_iterations):
+        sorted_data = data[data[:, axis].argsort()]
+        seg = data[0:(i + 1)*seg_size, :]
+        segs.append(seg)
+    return segs
