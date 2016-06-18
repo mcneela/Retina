280adc0d61396db4668f363bae5279596d900b14
mcneela <daniel.mcneela@gmail.com>
2016-06-17 22:35:17 -0700

    axes and nldr revisions

--- demos/nldr/swiss_roll.py
+++ demos/nldr/swiss_roll.py
@@ -1,5 +1,5 @@
 """
-Adopted from the swiss_roll.py example
+Adapted from the swiss_roll.py example
 packaged with Scikit-Learn.
 """
 import mapping
@@ -9,17 +9,39 @@ import numpy as np
 from matplotlib import gridspec
 from sklearn import manifold, datasets
 
-def mouse_over(event):
-    ax = event.inaxes
-    print(ax.name)
-    sec = nld.get_layer(ax.title._text)
-    sec.bound()
+class EventSystem:
+    def __init__(self, fig):
+        self.fig = fig
+        self.hover_sec = None
+        self.click_sec = None
+        self.fig.canvas.mpl_connect('motion_notify_event', self.mouse_over)
+        self.fig.canvas.mpl_connect('button_press_event', self.mouse_click)
+
+    def mouse_over(self, event):
+        ax = event.inaxes
+        try:
+            sec = nld.get_layer(ax.title._text)
+        except:
+            return
+        if sec is not self.hover_sec and self.hover_sec:
+            self.hover_sec.unbound()
+        sec.bound()
+        self.hover_sec = sec
+
+    def mouse_click(self, event):
+        ax = event.inaxes
+        try:
+            sec = nld.get_layer(ax.title._text)
+        except:
+            return
+        if sec is not self.click_sec and self.click_sec:
+            nld.showcase(sec.name)
+        self.click_sec = sec
+
 
 X, color = datasets.samples_generator.make_swiss_roll(n_samples=1500)
-print("Computing LLE embedding")
 X_r, err = manifold.locally_linear_embedding(X, n_neighbors=12,
                                              n_components=2)
-print("Done. Reconstruction error: %g" % err)
 
 dat_min = np.amin(X[:,1])
 dat_max = np.amax(X[:,1])
@@ -27,7 +49,6 @@ fig = plt.figure(figsize=(20, 20))
 gs = gridspec.GridSpec(2, 3)
 nld = plt.subplot(gs[0,0], projection='Fovea3D')
 
-fig.canvas.mpl_connect('motion_notify_event', mouse_over)
 num_sections = 5
 sections = mapping.section(X, num_sections, axis='z')
 colors = mapping.section(X, num_sections, axis='y')
@@ -39,9 +60,14 @@ for i, j, sec, clr in zip([0, 0, 1, 1, 1], range(num_sections), sections, colors
     dcolor = len(color) / 5.0
     swiss_sec.add_data(sec[:, 0], sec[:, 1], sec[:, 2])
     nld.build_layer(swiss_sec.name, c=color[j*dcolor:(j+1)*dcolor], plot=nld.scatter, vmin=dat_min, vmax=dat_max, cmap=plt.cm.Spectral)
-    ax.scatter(X_r[:, 0], X_r[:, 1], c=color[j*dcolor:(j+1)*dcolor], vmin=dat_min, vmax=dat_max, cmap=plt.cm.Spectral)
+    proj = ax.add_layer('section ' + str(j) + ' proj')
+    proj.add_data(X_r[:, 0], X_r[:, 1])
+    ax.build_layer(proj.name, plot=ax.scatter, c=color[j*dcolor:(j+1)*dcolor], vmin=dat_min, vmax=dat_max, cmap=plt.cm.Spectral)
     ax.set_title('section ' + str(j))
 
+handler = EventSystem(fig)
+print("Hover over a projected data plot to see the corresponding segment in the original Swiss Roll.")
+print("Click on a subplot to see the corresponding segment of the original Swiss Roll be showcased.")
 nld.set_title("Original data")
 plt.axis('tight')
 plt.xticks([]), plt.yticks([])
