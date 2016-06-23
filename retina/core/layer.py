492116ceb3a7757ecf007b44f65182bf4a9bf492
mcneela <daniel.mcneela@gmail.com>
2016-06-23 08:40:23 -0700

    Reinserted needed import statements

--- retina/core/layer.py
+++ retina/core/layer.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.axes import Axes
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.lines import Line2D
 from matplotlib.artist import *
 from matplotlib.patches import *
