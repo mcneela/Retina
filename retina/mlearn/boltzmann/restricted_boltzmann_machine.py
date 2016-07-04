2378832bbc78022af82739368096b433c7645cd7
mcneela <daniel.mcneela@gmail.com>
2016-07-03 19:03:57 -0700

    More boltzmann class methods

--- retina/mlearn/boltzmann/restricted_boltzmann_machine.py
+++ retina/mlearn/boltzmann/restricted_boltzmann_machine.py
@@ -7,13 +7,46 @@ class RBM(object):
     def __init__(self, num_visible, num_hidden):
         self.num_visible = num_visible
         self.num_hidden = num_hidden
+
+        # Set the network's weight matrix to all zeros, initially
         self.weights = np.zeros(self.num_visible, self.num_hidden)  
         
+        # Initialize the network's visible (binary) states
+        self.visible = np.zeros(self.num_visible, 1)
+
+        # Initialize the network's hidden (binary) states
+        self.hidden = np.zeros(self.num_hidden, 1)
+
         # Bias weights for the visible layer
-        self.a = np.random.logistic(size=(1, self.num_visible))
+        self.visible_bias = np.random.bernoulli(size=(self.num_visible, 1))
 
         # Bias weights for the hidden layer
-        self.b = np.random.logistic(size=(1, self.num_hidden))
+        self.hidden_bias = np.random.bernoulli(size=(self.num_hidden, 1))
 
     def energy(self):
-        E = -np.dot(a.T, 
+        E = -np.dot(self.visible_bias.T, self.visible) \
+            -np.dot(self.hidden_bias.T, self.hidden) \
+            -np.dot(self.visible.T, np.multiply(self.weights, self.hidden))
+        return E
+
+    def v_cond_h(self):
+        visible = []
+        for i in range(self.num_visible):
+            arg = self.visible[i] + np.dot(self.hidden, self.weights[i, :])
+            visible.append(sigmoid(arg))
+        return visible
+
+    def h_cond_v(self):
+        hidden = []
+        for j in range(self.num_hidden):
+            arg = self.hidden[j] + np.dot(self.visible, self.weights[:, j])
+            hidden.append(sigmoid(arg))
+        return hidden
+
+    def gibbs_sampling(self, num_iters):
+        for iter_num in num_iters:
+            self.hidden = self.h_cond_v()
+            self.visible = self.v_cond_h()
+    
+
+        
