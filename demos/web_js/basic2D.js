86961229d2176c22d8f47c7df97695627405d043
mcneela <daniel.mcneela@gmail.com>
2016-07-08 19:40:17 -0700

    Created web-based basic2D example

---
+++ demos/web_js/basic2D.js
@@ -0,0 +1,38 @@
+// This demo uses the library NumericJS for
+// numpy-like matrix computations.
+
+var base_x = numeric.linspace(-2, 2, 50);
+var quad_y = numeric.pow(base_x, 2);
+var cube_y = numeric.pow(base_x, 3);
+var quart_y = numeric.pow(base_x, 4);
+
+var div = document.getElementById('2Dplot');
+
+var quadratic = {
+	x: base_x,
+	y: quad_y,
+	mode: 'lines'
+};
+
+var cubic = {
+	x: base_x,
+	y: cube_y,
+	mode: 'lines'
+};
+
+var quartic = {
+	x: base_x,
+	y: quart_y,
+	mode: 'lines'
+};
+
+var evenPowers = new Layer("evenPowers", div);
+var oddPowers = new Layer("oddPowers", div);
+
+oddPowers.addTrace(cubic);
+evenPowers.addTrace(quadratic);
+evenPowers.addTrace(quartic);
+
+var traces = [quadratic, cubic, quartic];
+
+Plotly.plot(div, traces);
